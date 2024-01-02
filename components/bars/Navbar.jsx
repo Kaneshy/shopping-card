b00b5bbabe721248705c669341b54f0c8c6b2ad1
david <kaneshiuchiha@gmail.com>
2024-01-02 17:30:25 -0500

    first commit

---
+++ components/bars/Navbar.jsx
@@ -0,0 +1,56 @@
+'use client'
+import Link from "next/link"
+import { useState } from "react"
+import { FiMenu } from "react-icons/fi";
+import { RiMenu3Fill } from "react-icons/ri";
+
+const Navbar = () => {
+
+    const [isActive, setisActive] = useState(false)
+
+    return (
+        <main className="flex justify-between p-2 gap-x-4 select-none">
+            <section className="flex gap-x-2">
+                <img src="/assets/logo.svg" alt="" />
+                <label className="max-sm:hidden" htmlFor="img">PixelShopping</label>
+            </section>
+            <section className="flex justify-end w-full gap-x-8">
+                <div>
+                    <div className="flex justify-between gap-x-6 max-sm:hidden">
+                        <Link href={'/'} >Home</Link>
+                        <Link href={'/'} >About</Link>
+                        <Link href={'/'} >Contact</Link>
+                        <Link href={'/'} >Share</Link>
+                    </div>
+                    <div className="">
+                        <div className={`max-sm:flex hidden px-6 ${isActive && 'bg-neutral-900 rounded'}`} onClick={() => setisActive(!isActive)}>
+                            {isActive ? (
+                                <RiMenu3Fill size={25} className="text-neutral-400 hover:text-neutral-200" />
+                            ) : (
+                                <FiMenu size={25} className="text-neutral-400 hover:text-neutral-200" />
+                            )}
+                        </div>
+                        {isActive && (
+                            <div className=" fixed top-10 left-0 right-0 bg-neutral-900 p-4 rounded flex-col gap-y-2 max-sm:flex hidden">
+                                <section className="flex gap-x-2 hover:bg-neutral-600 p-2 rounded">
+                                    <img src="/assets/logo.svg" alt="" />
+                                    <p  >more</p>
+                                </section>
+                                <section className="flex gap-x-2 hover:bg-neutral-600 p-2 rounded">
+                                    <img src="/assets/logo.svg" alt="" />
+                                    <p  >setting</p>
+                                </section>
+                            </div>
+                        )}
+
+                    </div>
+                </div>
+                <div>
+                    <div>CARD</div>
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default Navbar
---
+++ constants/index.js
@@ -0,0 +1,80 @@
+export const sidebarLinks = [
+    {
+        imgURL: "/assets/home.svg",
+        route: "/Home",
+        label: "Home",
+    },
+    {
+        imgURL: "/assets/upload.png",
+        route: "/Upload",
+        label: "Upload",
+    },
+    {
+        imgURL: "/assets/setting.png",
+        route: "/User",
+        label: "User",
+    }
+];
+
+export const sidebarTrens = [
+    {
+        imgURL: "/assets/user.svg",
+        route: "/bytags",
+        label: "irl",
+    },
+    {
+        imgURL: "/assets/musical.png",
+        route: "/bytags",
+        label: "music",
+    },
+    {
+        imgURL: "/assets/search.svg",
+        route: "/bytags",
+        label: "learning",
+    },
+    {
+        imgURL: "/assets/heart.svg",
+        route: "/bytags",
+        label: "animation",
+    },
+    {
+        imgURL: "/assets/console.png",
+        route: "/bytags",
+        label: "Gaming",
+    },
+];
+
+export const recommendationesTags = [
+    {
+        route: "/bytags",
+        label: "irl",
+    },
+    {
+        route: "/bytags",
+        label: "music",
+    },
+    {
+        route: "/bytags",
+        label: "learning",
+    },
+    {
+        route: "/bytags",
+        label: "anime",
+    },
+    {
+        route: "/bytags",
+        label: "Gaming",
+    },
+    {
+        route: "/bytags",
+        label: "physics",
+    },
+    {
+        route: "/bytags",
+        label: "history",
+    },
+    {
+        route: "/bytags",
+        label: "cinema",
+    }
+];
